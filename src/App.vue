510caccbb15f5ef1f3b4538b9b9ffedfed133aa4
Pavel <ves.pavel94@gmail.com>
2019-06-15 17:26:37 +0300

    Lorem

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis ea nihil. Mollitia voluptates placeat quasi eaque recusandae, incidunt nostrum nobis vero nemo necessitatibus soluta iste itaque voluptatum, tempore perferendis?</p>
   </div>
 </template>
 
